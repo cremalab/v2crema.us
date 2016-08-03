576f4c0fe3915fb9cc75f1155c8f98a1f7195ffc
Chris <walter.chris@gmail.com>
2016-08-03 16:53:33 -0500

    images added

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,8 +1,34 @@
+import $ from 'jquery'
 
-
-var waypoint = new Waypoint({
-  element: document.getElementById('showcase1'),
-  handler: function(direction) {
-    console.log('Scrolled to waypoint!')
+$(document).ready(function(){
+  
+  function draw() {
+        requestAnimationFrame(draw)
+        scrollEvent()
   }
+  draw()
+ 
 })
+ 
+function scrollEvent(){
+  
+  if(!is_touch_device()){
+    let sym
+    let viewportTop = $(window).scrollTop()
+    let windowHeight = $(window).height()
+    let viewportBottom = windowHeight + viewportTop
+    if($(window).width())
+
+    $('[data-parallax="true"]').each(function(){
+      let distance = viewportTop * $(this).attr('data-speed');
+      if($(this).attr('data-direction') === 'up'){sym = '-' } else { sym = '' }
+      $(this).css('transform','translate3d(0, ' + sym + distance +'px,0)')
+    })
+    
+  }
+}   
+ 
+function is_touch_device() {
+  return 'ontouchstart' in window // works on most browsers 
+      || 'onmsgesturechange' in window; // works on ie10
+}
