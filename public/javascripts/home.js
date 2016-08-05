3c5be869cb1f53546b5e6c4ecf807e5ff2e67a05
Chris <walter.chris@gmail.com>
2016-08-05 12:21:52 -0500

    fade working

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,8 +1,11 @@
 import './_home-parallax'
 import 'waypoints/lib/noframework.waypoints.js'
-import 'Blazy'
- var bLazy = new Blazy();
-console.log(bLazy)
+// import 'blazy'
+
+ var bLazy = new Blazy({
+   successClass: 'img-fadein'
+ })
+
 //turn crema video off and on
 let cremaVid = document.getElementById('crema-vid-logo')
 new Waypoint({
@@ -18,7 +21,7 @@ new Waypoint({
     },  
 })
 
-//load imgs
+
 
 
 
