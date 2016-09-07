2f279dba7f95e1ff28ff98ece23451c324121b99
Chris <walter.chris@gmail.com>
2016-09-07 10:45:57 -0500

    service mobile popup

--- public/javascripts/services-mobile.js
+++ public/javascripts/services-mobile.js
@@ -3,6 +3,7 @@
 
 import $ from 'jquery'
 import 'slick-carousel'
+import 'magnific-popup'
 
 
 let serviceTextMobile = []
@@ -11,7 +12,7 @@ $('.mobile-vid-wrapper').slick({
   dots: true,
   speed: 300,
   centerMode: true,
-  infinite: false
+  infinite: true
 })
 
 $('.mobile-vid-wrapper').on('swipe', function(event, slick, direction){
@@ -24,6 +25,33 @@ $('.mobile-vid-wrapper').on('swipe', function(event, slick, direction){
 })
 
 $(document).ready( () => {  
+
+$('.mobile-vid-wrapper').magnificPopup({
+      delegate: 'a',
+      type: 'iframe',
+      alignTop: true,
+      iframe: {
+        markup: '<div class="mfp-iframe-scaler">'+
+                  '<div class="mfp-close"></div>'+
+                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+        patterns: {
+          youtube: {
+            index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+            id: 'v=', // String that splits URL in a two parts, second part should be %id%
+            // Or null - full URL will be returned
+            // Or a function that should return %id%, for example:
+            // id: function(url) { return 'parsed id'; }
+            src: '//www.youtube.com/embed/%id%?controls=0&modestbranding=1&showinfo=0&iv_load_policy=3&enablejsapi=1?controls=0&modestbranding=1&autoplay=1&showinfo=0&iv_load_policy=3&enablejsapi=1' // URL that will be set as a source for iframe.
+          }
+        }
+      }
+      
+      
+    })    
+      
+
   addPlayBtnListeners('mobile-service-vid')
   serviceTextMobile = $('.service-text-mobile')
   serviceTextMobile[0].style.opacity = 1 
@@ -36,23 +64,14 @@ function addPlayBtnListeners(className) {
     console.log(btns)
     for (let i =0; i < btns.length; i++){
       btns[i].addEventListener('click', initServiceVideo.bind(btns[i]))  
-    }
-    
+    } 
   }
   
 
   function initServiceVideo(){
-    console.log(this.id)
-    let vid = this.getElementsByTagName('iframe')[0]
+    console.log('click')
     let btn = this.getElementsByClassName('play-btn-svg')[0]
     let spinner = this.getElementsByClassName('crema-spinner')[0]
-    btn.style.zIndex = -1
     btn.style.animationName = 'crema-spinner-rev'
     spinner.style.animationName = 'crema-spinner'
-    spinner.style.zIndex = -1
-    console.log(btn)
-    if (!vid.dataset.added) {
-      vid.src = vid.dataset.src
-      vid.dataset.added = 'true'
-    }     
   }
