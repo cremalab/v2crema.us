261d6ddc8b0cd034c7e56c973c86de0fc2acf4b5
chriswphoto <walter.chris@gmail.com>
2017-02-08 11:24:59 -0600

    animating cards copy

--- public/javascripts/build/home.js
+++ public/javascripts/build/home.js
@@ -10060,103 +10060,114 @@
 	    }
 	  });
 
-	  var top_mask = document.getElementById('showcase-mask-top');
-	  var bot_mask = document.getElementById('showcase-mask-bot');
-	  var taglines = document.getElementById('taglines');
+	  // let top_mask  = document.getElementById('showcase-mask-top')
+	  // let bot_mask  = document.getElementById('showcase-mask-bot')
+	  // let taglines = document.getElementById('taglines')
 	  var tagline_1 = document.getElementById('tagline-1');
+	  var tagline_2 = document.getElementById('tagline-2');
+	  var tagline_3 = document.getElementById('tagline-3');
+	  var tagline_4 = document.getElementById('tagline-4');
 
 	  // top-showcase-mask waypoint
 	  new Waypoint({
-	    element: document.getElementById('cards-wrapper'),
+	    element: document.getElementById('trigger-copy-change-1'),
 	    handler: function handler(direction) {
 	      if (direction == 'down') {
-	        // top_mask.classList.remove('showcase-mask-top-start')
-	        // top_mask.classList.add('showcase-mask-top-scroll')
-	        // bot_mask.classList.remove('showcase-mask-bot-start')
-	        // bot_mask.classList.add('showcase-mask-bot-scroll')
-	        taglines.classList.remove('taglines-start');
-	        taglines.classList.add('taglines-scroll');
+	        tagline_1.classList.add('tagline-animate-in-down');
 	      } else {
-	        // top_mask.classList.remove('showcase-mask-top-scroll')
-	        // top_mask.classList.add('showcase-mask-top-start')
-	        // bot_mask.classList.remove('showcase-mask-bot-scroll')
-	        // bot_mask.classList.add('showcase-mask-bot-start')
-	        taglines.classList.remove('taglines-scroll');
-	        taglines.classList.add('taglines-start');
+	        tagline_1.classList.remove('tagline-animate-in-down');
+	        tagline_1.classList.remove('tagline-animate-out');
+	        tagline_2.classList.remove('tagline-animate-in-down');
 	      }
 	    }
 	  });
 
 	  // bot-showcase-mask waypoint
 	  new Waypoint({
-	    element: document.getElementById('waypoints-logos'),
-	    handler: function handler(direction) {
-	      if (direction == 'down') {
-	        // top_mask.classList.remove('showcase-mask-top-scroll')
-	        // top_mask.classList.add('showcase-mask-top-end')
-	        bot_mask.classList.remove('showcase-mask-bot-scroll');
-	        bot_mask.classList.add('showcase-mask-bot-end');
-	        taglines.classList.remove('taglines-scroll');
-	        taglines.classList.add('taglines-end');
-	      } else {
-	        // top_mask.classList.remove('showcase-mask-top-end')
-	        // top_mask.classList.add('showcase-mask-top-scroll')
-	        // bot_mask.classList.remove('showcase-mask-bot-end')
-	        // bot_mask.classList.add('showcase-mask-bot-scroll')
-	        taglines.classList.remove('taglines-end');
-	        taglines.classList.add('taglines-scroll');
-	      }
-	    },
-	    offset: window.innerHeight
-	  });
-
-	  // First triger to change language
-	  new Waypoint({
-	    element: document.getElementById('trigger-copy-change-1'),
-	    handler: function handler(direction) {
-	      var tagline_1 = document.getElementById('tagline-1');
-	      var tagline_2 = document.getElementById('tagline-2');
-	      if (direction == 'down') {
-	        tagline_1.classList.add('-opacity-0');
-	        tagline_2.classList.remove('-opacity-0');
-	      } else {
-	        tagline_1.classList.remove('-opacity-0');
-	        tagline_2.classList.add('-opacity-0');
-	      }
-	    }
-	  });
-
-	  // Second triger to change language
-	  new Waypoint({
 	    element: document.getElementById('trigger-copy-change-2'),
 	    handler: function handler(direction) {
-	      var tagline_2 = document.getElementById('tagline-2');
-	      var tagline_3 = document.getElementById('tagline-3');
 	      if (direction == 'down') {
-	        tagline_2.classList.add('-opacity-0');
-	        tagline_3.classList.remove('-opacity-0');
+	        // tagline_1.classList.add('tagline-animate-out-left')
+	        tagline_1.classList.add('tagline-animate-out');
+	        tagline_1.classList.remove('tagline-animate-in-down');
+	        tagline_2.classList.remove('tagline-animate-out');
+	        tagline_2.classList.add('tagline-animate-in-down');
 	      } else {
-	        tagline_2.classList.remove('-opacity-0');
-	        tagline_3.classList.add('-opacity-0');
+	        tagline_2.classList.remove('tagline-animate-in-down');
+	        tagline_2.classList.add('tagline-animate-out');
+	        tagline_1.classList.add('tagline-animate-in-down');
+	        tagline_1.classList.remove('tagline-animate-out');
 	      }
 	    }
 	  });
-
 	  // third triger to change language
 	  new Waypoint({
 	    element: document.getElementById('trigger-copy-change-3'),
 	    handler: function handler(direction) {
-	      var tagline_3 = document.getElementById('tagline-3');
-	      var tagline_4 = document.getElementById('tagline-4');
 	      if (direction == 'down') {
-	        tagline_3.classList.add('-opacity-0');
-	        tagline_4.classList.remove('-opacity-0');
+	        tagline_2.classList.add('tagline-animate-out');
+	        tagline_2.classList.remove('tagline-animate-in-down');
+	        tagline_3.classList.remove('tagline-animate-out');
+	        tagline_3.classList.add('tagline-animate-in-down');
 	      } else {
-	        tagline_3.classList.remove('-opacity-0');
-	        tagline_4.classList.add('-opacity-0');
+	        tagline_3.classList.remove('tagline-animate-in-down');
+	        tagline_3.classList.add('tagline-animate-out');
+	        tagline_2.classList.add('tagline-animate-in-down');
+	        tagline_2.classList.remove('tagline-animate-out');
 	      }
 	    }
 	  });
+	  // third triger to change language
+	  new Waypoint({
+	    element: document.getElementById('trigger-copy-change-4'),
+	    handler: function handler(direction) {
+	      if (direction == 'down') {
+	        tagline_3.classList.add('tagline-animate-out');
+	        tagline_3.classList.remove('tagline-animate-in-down');
+	        tagline_4.classList.add('tagline-animate-in-down');
+	      } else {
+	        tagline_3.classList.remove('tagline-animate-out');
+	        tagline_3.classList.add('tagline-animate-in-down');
+	        tagline_4.classList.remove('tagline-animate-in-down');
+	        tagline_4.classList.add('tagline-animate-out');
+	      }
+	    }
+	  });
+
+	  // // First triger to change language
+	  // new Waypoint({
+	  //   element: document.getElementById('trigger-copy-change-1'), 
+	  //   handler: function(direction) {
+	  //     let tagline_1 = document.getElementById('tagline-1')
+	  //     let tagline_2 = document.getElementById('tagline-2')
+	  //     if (direction == 'down') {
+	  //       tagline_1.classList.add('-opacity-0')
+	  //       tagline_2.classList.remove('-opacity-0')
+	  //     } else {
+	  //       tagline_1.classList.remove('-opacity-0')
+	  //       tagline_2.classList.add('-opacity-0')
+	  //     }
+	  //   },
+	  //   // offset: window.innerHeight * .5  
+	  // })
+
+	  // // Second triger to change language
+	  // new Waypoint({
+	  //   element: document.getElementById('trigger-copy-change-2'), 
+	  //   handler: function(direction) {
+	  //     let tagline_2 = document.getElementById('tagline-2')
+	  //     let tagline_3 = document.getElementById('tagline-3')
+	  //     if (direction == 'down') {
+	  //       tagline_2.classList.add('-opacity-0')
+	  //       tagline_3.classList.remove('-opacity-0')
+	  //     } else {
+	  //       tagline_2.classList.remove('-opacity-0')
+	  //       tagline_3.classList.add('-opacity-0')
+	  //     }
+	  //   },
+	  //   // offset: window.innerHeight * .5  
+	  // })
+
 	});
 
 /***/ }
--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -15,105 +15,116 @@ $(document).ready(()=>{
     }
   })
   
-  let top_mask  = document.getElementById('showcase-mask-top')
-  let bot_mask  = document.getElementById('showcase-mask-bot')
-  let taglines = document.getElementById('taglines')
+  // let top_mask  = document.getElementById('showcase-mask-top')
+  // let bot_mask  = document.getElementById('showcase-mask-bot')
+  // let taglines = document.getElementById('taglines')
   let tagline_1 = document.getElementById('tagline-1')
+  let tagline_2 = document.getElementById('tagline-2')
+  let tagline_3 = document.getElementById('tagline-3')
+  let tagline_4 = document.getElementById('tagline-4')
    
   // top-showcase-mask waypoint
   new Waypoint({
-    element: document.getElementById('cards-wrapper'), 
+    element: document.getElementById('trigger-copy-change-1'), 
     handler: function(direction) {
       if (direction == 'down') {
-        // top_mask.classList.remove('showcase-mask-top-start')
-        // top_mask.classList.add('showcase-mask-top-scroll')
-        // bot_mask.classList.remove('showcase-mask-bot-start')
-        // bot_mask.classList.add('showcase-mask-bot-scroll')
-        taglines.classList.remove('taglines-start')
-        taglines.classList.add('taglines-scroll')
+        tagline_1.classList.add('tagline-animate-in-down')
       } else {
-        // top_mask.classList.remove('showcase-mask-top-scroll')
-        // top_mask.classList.add('showcase-mask-top-start')
-        // bot_mask.classList.remove('showcase-mask-bot-scroll')
-        // bot_mask.classList.add('showcase-mask-bot-start')
-        taglines.classList.remove('taglines-scroll')
-        taglines.classList.add('taglines-start')
+        tagline_1.classList.remove('tagline-animate-in-down')
+        tagline_1.classList.remove('tagline-animate-out')
+        tagline_2.classList.remove('tagline-animate-in-down')
       }
     }  
   })
 
   // bot-showcase-mask waypoint
   new Waypoint({
-    element: document.getElementById('waypoints-logos'), 
-    handler: function(direction) {
-      if (direction == 'down') {
-        // top_mask.classList.remove('showcase-mask-top-scroll')
-        // top_mask.classList.add('showcase-mask-top-end')
-        bot_mask.classList.remove('showcase-mask-bot-scroll')
-        bot_mask.classList.add('showcase-mask-bot-end')
-        taglines.classList.remove('taglines-scroll')
-        taglines.classList.add('taglines-end')
-      } else {
-        // top_mask.classList.remove('showcase-mask-top-end')
-        // top_mask.classList.add('showcase-mask-top-scroll')
-        // bot_mask.classList.remove('showcase-mask-bot-end')
-        // bot_mask.classList.add('showcase-mask-bot-scroll')
-        taglines.classList.remove('taglines-end')
-        taglines.classList.add('taglines-scroll')
-      }
-    },
-    offset: window.innerHeight  
-  })
-  
-  // First triger to change language
-  new Waypoint({
-    element: document.getElementById('trigger-copy-change-1'), 
-    handler: function(direction) {
-      let tagline_1 = document.getElementById('tagline-1')
-      let tagline_2 = document.getElementById('tagline-2')
-      if (direction == 'down') {
-        tagline_1.classList.add('-opacity-0')
-        tagline_2.classList.remove('-opacity-0')
-      } else {
-        tagline_1.classList.remove('-opacity-0')
-        tagline_2.classList.add('-opacity-0')
-      }
-    },
-    // offset: window.innerHeight * .5  
-  })
-  
-  // Second triger to change language
-  new Waypoint({
     element: document.getElementById('trigger-copy-change-2'), 
     handler: function(direction) {
-      let tagline_2 = document.getElementById('tagline-2')
-      let tagline_3 = document.getElementById('tagline-3')
       if (direction == 'down') {
-        tagline_2.classList.add('-opacity-0')
-        tagline_3.classList.remove('-opacity-0')
+        // tagline_1.classList.add('tagline-animate-out-left')
+        tagline_1.classList.add('tagline-animate-out')
+        tagline_1.classList.remove('tagline-animate-in-down')
+        tagline_2.classList.remove('tagline-animate-out')
+        tagline_2.classList.add('tagline-animate-in-down')
       } else {
-        tagline_2.classList.remove('-opacity-0')
-        tagline_3.classList.add('-opacity-0')
+        tagline_2.classList.remove('tagline-animate-in-down')
+        tagline_2.classList.add('tagline-animate-out')
+        tagline_1.classList.add('tagline-animate-in-down')
+        tagline_1.classList.remove('tagline-animate-out')
+        
       }
     },
-    // offset: window.innerHeight * .5  
+    // offset: window.innerHeight  
   })
-  
-  // third triger to change language
+   // third triger to change language
   new Waypoint({
     element: document.getElementById('trigger-copy-change-3'), 
     handler: function(direction) {
-      let tagline_3 = document.getElementById('tagline-3')
-      let tagline_4 = document.getElementById('tagline-4')
       if (direction == 'down') {
-        tagline_3.classList.add('-opacity-0')
-        tagline_4.classList.remove('-opacity-0')
+        tagline_2.classList.add('tagline-animate-out')
+        tagline_2.classList.remove('tagline-animate-in-down')
+        tagline_3.classList.remove('tagline-animate-out')
+        tagline_3.classList.add('tagline-animate-in-down')
       } else {
-        tagline_3.classList.remove('-opacity-0')
-        tagline_4.classList.add('-opacity-0')
+        tagline_3.classList.remove('tagline-animate-in-down')
+        tagline_3.classList.add('tagline-animate-out')
+        tagline_2.classList.add('tagline-animate-in-down')
+        tagline_2.classList.remove('tagline-animate-out')
       }
     },
-    // offset: window.innerHeight * .5  
   })
+   // third triger to change language
+  new Waypoint({
+    element: document.getElementById('trigger-copy-change-4'), 
+    handler: function(direction) {
+      if (direction == 'down') {
+        tagline_3.classList.add('tagline-animate-out')
+        tagline_3.classList.remove('tagline-animate-in-down')
+        tagline_4.classList.add('tagline-animate-in-down')
+      } else {
+        tagline_3.classList.remove('tagline-animate-out')
+        tagline_3.classList.add('tagline-animate-in-down')
+        tagline_4.classList.remove('tagline-animate-in-down')
+        tagline_4.classList.add('tagline-animate-out')
+      }
+    },
+  })
+  
+  // // First triger to change language
+  // new Waypoint({
+  //   element: document.getElementById('trigger-copy-change-1'), 
+  //   handler: function(direction) {
+  //     let tagline_1 = document.getElementById('tagline-1')
+  //     let tagline_2 = document.getElementById('tagline-2')
+  //     if (direction == 'down') {
+  //       tagline_1.classList.add('-opacity-0')
+  //       tagline_2.classList.remove('-opacity-0')
+  //     } else {
+  //       tagline_1.classList.remove('-opacity-0')
+  //       tagline_2.classList.add('-opacity-0')
+  //     }
+  //   },
+  //   // offset: window.innerHeight * .5  
+  // })
+  
+  // // Second triger to change language
+  // new Waypoint({
+  //   element: document.getElementById('trigger-copy-change-2'), 
+  //   handler: function(direction) {
+  //     let tagline_2 = document.getElementById('tagline-2')
+  //     let tagline_3 = document.getElementById('tagline-3')
+  //     if (direction == 'down') {
+  //       tagline_2.classList.add('-opacity-0')
+  //       tagline_3.classList.remove('-opacity-0')
+  //     } else {
+  //       tagline_2.classList.remove('-opacity-0')
+  //       tagline_3.classList.add('-opacity-0')
+  //     }
+  //   },
+  //   // offset: window.innerHeight * .5  
+  // })
+  
+ 
 
 })
